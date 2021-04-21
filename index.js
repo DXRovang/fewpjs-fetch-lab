08d128e57e0f28af5f10e9801d14980e5062817c
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-20 20:31:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const url = 'https://anapioficeandfire.com/api/books'
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch(url).then(resp => resp.json()).then(function(data){
+    console.log(data)
+    debugger
+    books = []
+    for (i = 0; i < data.length; i++){
+      books.push(data[i].name)
+    }
+    return books
+  })
 }
 
 function renderBooks(books) {
